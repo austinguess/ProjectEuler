9fc798e6eb2d3a65cc472e4f1303ed4ece7a56d3
Austin Guess <ausitn.guess@gmail.com>
2012-02-22 21:21:58 -0500

    finalized problem3 and moved to completed

--- completed/problem3.cpp
+++ completed/problem3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -12,11 +13,11 @@ int main ()
 	cout<<"The highest prime factor for 600851475143 is "<<primeFactor(600851475143)<<endl;
 	return 0;
 }
-
+//looks for factors and sums prime ones
 long int primeFactor (long int total)
 {
 	long int sum = 0;
-	long int current=total/2;
+	long int current=sqrt(total);
 	
 
 	for (long int i = 2; sum==0; i++)
@@ -27,13 +28,13 @@ long int primeFactor (long int total)
 
 	return sum;
 }
-
+//returns true if number is prime
 bool isPrime (long int number)
 
 {
 	bool toReturn = true;
 
-	for (long int i = 2; i < number/2; i++)	
+	for (long int i = 2; i < sqrt(number); i++)	
 	{
 		if (number%i == 0)
 			toReturn = false;
